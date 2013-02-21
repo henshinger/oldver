eb915af8d347783835442d4fb6e407cfdd369578
Kenley Tan <kenleytan@yahoo.com>
2013-02-21 19:59:48 +0800

    column adding proto

--- FlightAgency_4E_Group3/Search.vb
+++ FlightAgency_4E_Group3/Search.vb
@@ -1,6 +1,5 @@
 Public Class frmSearch
 
-
     Private Sub btnEdit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEdit.Click
         Dim strTable As String
         Me.Hide()
@@ -24,10 +23,28 @@ Public Class frmSearch
 
 
     Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
-
+        clstFilters.Items.Add(cboColumn.SelectedItem & " = " & cboVal.Text)
     End Sub
 
     Private Sub cboType_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cboType.SelectedIndexChanged
-
+        Dim userCol() As String = {"FirstName", "LastName", "Gender", "Birthday"}
+        Dim seatCol() As String = {"SeatNo"}
+        Dim flightCol() As String = {"Airline", "Status"}
+        Select Case cboType.SelectedItem
+            Case "User"
+                For Each col As String In userCol
+                    cboColumn.Items.Add(col)
+                Next
+            Case "Seat"
+                For Each col As String In seatCol
+                    cboColumn.Items.Add(col)
+                Next
+            Case "Flight"
+                For Each col As String In flightCol
+                    cboColumn.Items.Add(col)
+                Next
+            Case Else
+                MessageBox.Show("Please specify what to search for")
+        End Select
     End Sub
 End Class
